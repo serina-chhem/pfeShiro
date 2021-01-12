4ad0b4bfb170a5c0432b21a223bd72dd042d6300
Nathan <nathan.lancman@edu.ece.fr>
2021-01-12 12:09:58 +0100

    stuff

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask
-
+from flask import Flask, render_template
+import speech_recognition as sr
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template("index.html")
 
+@app.route('/stt')
+def stt():
+    transcript = ""
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print('Parlez')
+        audio = r.listen(source)
+        transcript = r.recognize_google(audio)
+        print("Vous dites : " + transcript)
+    return render_template("stt.html", transcript = transcript)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug = True, threaded = True)
